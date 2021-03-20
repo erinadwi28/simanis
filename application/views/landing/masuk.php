6a1c2d9cc86389d733faba841b8076834cc58d13
ahmads <ahmadsyaebani1@gmail.com>
2021-03-20 21:24:54 +0700

    bani_daftar dan login pengguna

--- application/views/landing/masuk.php
+++ application/views/landing/masuk.php
@@ -11,29 +11,48 @@
 							<h3>Masuk</h3>
 							<p class="mb-4">masuk ke akun Anda untuk menggunakan layanan ini.</p>
 						</div>
-						<form action="#" method="post">
+
+						<?php if ($this->session->flashdata('success')) : ?>
+							<div class="alert alert-success alert-dismissible fade show" role="alert">
+								<?= $this->session->flashdata('success') ?>
+								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+						<?php elseif ($this->session->flashdata('error')) : ?>
+							<div class="alert alert-danger alert-dismissible fade show" role="alert">
+								<?= $this->session->flashdata('error') ?>
+								<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+						<?php endif ?>
+
+						<form action="<?= base_url('masuk/aksi_login'); ?>" method="post">
 							<div class="form-group first shadow">
 								<label for="email">Email | contoh@email.com</label>
-								<input type="email" class="form-control" id="email">
+								<input type="email" class="form-control" id="email" name="email">
 							</div>
 							<div class="form-group last mb-4 shadow">
 								<label for="kata_sandi">Kata Sandi</label>
-								<input type="kata_sandi" class="form-control" id="kata_sandi">
+								<input type="password" class="form-control" id="kata_sandi" name="kata_sandi">
 							</div>
 
 							<div class="d-flex mb-3 align-items-center">
 								<span class="ml-auto"><a href="#" class="lupa_sandi"> <small> Lupa Kata Sandi
 											?</small></a></span>
 							</div>
+							<div class="g-recaptcha mb-2" data-sitekey="6LcWJXwaAAAAAJpeWzQao7fZOgv3zBsUTCLH4t0b">
+							</div>
 
 							<input type="submit" value="Masuk" class="btn btn-block btn-primary">
 							<center class="mt-3">
 								<small class="daftar">
-									<i class="fas fa-user"></i> Belum punya akun ? <a href="<?= base_url('daftar')?>">Daftar Sekarang</a>
+									<i class="fas fa-user"></i> Belum punya akun ? <a href="<?= base_url('daftar') ?>">Daftar Sekarang</a>
 								</small>
 							</center>
 							<center class="mt-1">
-								<a href="<?= base_url('beranda')?>"> <small>
+								<a href="<?= base_url('beranda') ?>"> <small>
 										<i class="fas fa-arrow-left"></i> Kembali Ke Beranda
 									</small>
 
