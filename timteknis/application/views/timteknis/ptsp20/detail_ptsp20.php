2753f40ee657b5f52c762e814b1929df30cb34e8
nadiaoktaria <nadia.05@amikom.ac.id>
2021-04-21 20:45:49 +0700

    nadia

--- timteknis/application/views/timteknis/ptsp20/detail_ptsp20.php
+++ timteknis/application/views/timteknis/ptsp20/detail_ptsp20.php
@@ -62,6 +62,7 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="col-md-8 mb-0">
 			<!-- Detail Data -->
 			<div class="card shadow mb-4">
@@ -189,16 +190,78 @@
 								<td> </td>
 								<td></td>
 							</tr>
-							
 						</tbody>
 					</table>
 				</div>
-
-				
 			</div>
 		</div>
 	</div>
+
+<!-- Unggah Berita Acara -->
+<div class="row clearfix">
+		<div class="col-xs-12 col-sm-6"></div>
+		<div class="col-xs-8 mb-0">
+			<div class="card shadow mb-4">
+				<div class="card-header">
+					<center>
+						<h6 class="m-0 font-weight-bold">Unggah Berita Acara terlebih dahulu untuk menyetujui</h6>
+					</center>
+				</div>
+
+				<div class="card-footer">
+					<form action="" enctype="multipart/form-data" method="post" accept-charset="utf-8" id="formupload_ptsp07_1">
+						<div class="form-group row">
+							<label for="nama" class="col-sm-3 col-form-label">No. Statsitik</label>
+							<div class="col-sm-9">
+								<div class="form-line focused">
+									<input type="text" class="form-control" id="nama" name="nama" value="" required>
+								</div>
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="no_hp" class="col-sm-3 col-form-label">Berita Acara Dokumen</label>
+							<div class="col-sm-9">
+								<div class="form-line focused">
+									<div class="input-group">
+										<div class="custom-file">
+											<label class="custom-file-label" for="file-upload">pilih
+												file...</label>
+											<input type="file" class="custom-file-input" id="file-upload" name="srt_permohonan"
+												value="" required>
+											<input type="hidden" class="form-control form-user-input" name="id_permohonan_ptsp"
+												id="file-upload" value="">
+										</div>
+										<div class="input-group-append">
+											<button class="btn btn-sm btn-primary" type="submit" id="inputGroupFileAddon04">
+											<i class="fa fa-upload"></i></button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Button Tolak & Setujui Awal Surat Masuk -->
+	<div class="row clearfix float-right px-2">
+		
+		<a href="" class="mr-2">
+			<button id=" btn_tolak" class="btn btn-sm btn-tolak" type="submit">
+				<i class="fas fa-times-circle">
+				</i> Tolak
+			</button>
+		</a>
+		<a href="">
+			<button id="btn_termia" class="btn btn-sm btn-primary" type="submit">
+				<i class="fas fa-check-circle">
+				</i> Terima
+			</button>
+		</a>
 	
+	</div>
 	
 	<!--End Content Profile-->
 </div>
