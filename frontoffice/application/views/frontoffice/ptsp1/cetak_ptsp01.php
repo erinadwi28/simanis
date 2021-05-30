7018d83061576db1258d54d23ed80e1c8b0579d3
erinadwi28 <erina.28@students.amikom.ac.id>
2021-05-30 07:53:27 +0700

    fix bug ptsp 1-9

--- frontoffice/application/views/frontoffice/ptsp1/cetak_ptsp01.php
+++ frontoffice/application/views/frontoffice/ptsp1/cetak_ptsp01.php
@@ -189,15 +189,17 @@
 				</tr>
 				<table width="530">
 					<tr>
-						<td class="text2"><?= format_indo(date($detail->tgl_persetujuan_kasubag)); ?></td>
+						<td class="text2"></td>
 					</tr>
 				</table>
 			</table>
 
 			<table>
 				<tr class="text2">
-					<td width="40">Nomer</td>
+					<td width="40">Nomor</td>
 					<td>: <?= $detail->no_surat ?></td>
+					<td width="200"> </td>
+					<td class="text2"><?= format_indo(date($detail->tgl_persetujuan_kasubag)); ?></td>
 				</tr>
 				<tr>
 					<td>Sifat</td>
